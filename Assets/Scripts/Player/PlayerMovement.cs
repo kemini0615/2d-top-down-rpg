c38c55237ba0ed259acc2b36a5dace68ab239ab6
kemini0615 <moeday1010@gmail.com>
2026-07-11 12:01:05 +0900

    Player: 플레이어 이동 애니메이션 구현

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,12 +7,14 @@ public class PlayerMovement : MonoBehaviour
 
     private PlayerActions actions;
     private Rigidbody2D rb;
+    private Animator animator;
     private Vector2 moveDirection;
 
     private void Awake()
     {
         actions = new PlayerActions();
         rb = GetComponent<Rigidbody2D>(); // 현재 게임 오브젝트에서 컴포넌트를 찾아 반환
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -30,6 +32,14 @@ public class PlayerMovement : MonoBehaviour
         // 'Movement' 액션 맵의 'Move' 액션 값 읽기
         // 'Move' 액션의 Action Type은 Value, Control Type은 Vector2
         moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+
+        // 아무런 유저 입력이 없을 때를 제외하고(마지막으로 이동한 방향을 유지하기 위해)
+        if (moveDirection != Vector2.zero)
+        {
+            // 애니메이터 파라미터를 갱신하여 통해 재생 애니메이션 변경
+            animator.SetFloat("xInput", moveDirection.x);
+            animator.SetFloat("yInput", moveDirection.y);
+        }
     }
 
     private void Move()
